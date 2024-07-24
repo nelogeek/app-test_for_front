b13be38d9326e4e059b8d0366c4de34a69e00578
Vladislav Lokotaev <lokotaevpr@gmail.com>
2024-07-24 14:21:04 +0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import envCompatible from 'vite-plugin-env-compatible';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/app-test_for_front/',
   plugins: [
     vue(),
+    envCompatible()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // Конфигурация статических ресурсов
   assetsInclude: ['**/*.png']
-})
+});
